e74cd82031b1fc5fe0b12ebb26c31535dff1b396
Jeongeun-Choi <cje3080@gmail.com>
2026-01-07 23:57:45 +0900

    feat(input): Input 컴포넌트 구현

---
+++ packages/ui/src/components/Input/index.tsx
@@ -0,0 +1,8 @@
+import { InputHTMLAttributes } from "react";
+import { inputStyle } from "./styles.css";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export default function Input(props: InputProps) {
+  return <input className={inputStyle} {...props} />;
+}
