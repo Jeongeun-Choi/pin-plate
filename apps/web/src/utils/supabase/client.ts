8d1fb7dcd55329062fd608efe40deca5cd0a7382
jeongeun <cje3080@gmail.com>
2026-01-26 23:13:56 +0900

    fix(login): supabase client 환경변수 수정

--- apps/web/src/utils/supabase/client.ts
+++ apps/web/src/utils/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr';
 
 export function createClient() {
   return createBrowserClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_API_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_API_KEY!,
   );
 }
