3693b9eb7e1f1ed3061ce6825874db5a18a25c2a
jeongeun <cje3080@gmail.com>
2026-01-27 00:05:31 +0900

    feat(map): naver map api 연동

--- apps/web/src/app/home/page.tsx
+++ apps/web/src/app/home/page.tsx
@@ -1,17 +1,5 @@
+import { Map } from '@/features/map/components/Map';
+
 export default function Home() {
-  return (
-    <main
-      style={{
-        width: '100vw',
-        height: '100dvh',
-        position: 'relative',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#eee',
-      }}
-    >
-      <h1>Map removed</h1>
-    </main>
-  );
+  return <Map />;
 }
