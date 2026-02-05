d9a5b32f0fbbe805843126be17492cbbe374afb1
jeongeun <cje3080@gmail.com>
2026-02-05 21:39:07 +0900

    feat(rate): Rate 공통 컴포넌트 생성

--- packages/ui/src/components/Input/styles.css.ts
+++ packages/ui/src/components/Input/styles.css.ts
@@ -1,18 +1,17 @@
-import { style } from "@vanilla-extract/css";
-import { vars } from "../../styles/vars.css";
+import { style } from '@vanilla-extract/css';
+import { vars } from '../../styles/vars.css';
 
 export const inputStyle = style({
-  display: "flex",
-  flex: 1,
+  display: 'flex',
   color: vars.colors.text.primary,
   padding: `${vars.spacing[1]} ${vars.spacing[5]}`,
   borderRadius: vars.borderRadius.sm,
   border: `1px solid ${vars.colors.secondary.border}`,
-  cursor: "pointer",
+  cursor: 'pointer',
   fontSize: vars.fontSize.tiny,
   fontWeight: vars.fontWeight.medium,
 
-  ":focus-visible": {
+  ':focus-visible': {
     outline: `1px solid ${vars.colors.primary.default}`,
   },
 });
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,4 +2,5 @@ export { vars } from './styles/vars.css';
 export * from './components/Button';
 export * from './components/Input';
 export * from './components/Textarea';
+export * from './components/Rate';
 export * from './icons';
