92decca3cff211992b28ddbf300913ed4fa8bdd2
jeongeun <cje3080@gmail.com>
2026-05-11 23:50:08 +0900

    feat(ui): IcBookmark, IcFilledBookmark 아이콘 추가

---
+++ packages/ui/src/icons/IcBookmark.tsx
@@ -0,0 +1,22 @@
+import { SVGProps } from 'react';
+
+export const IcBookmark = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M4.33333 2H11.6667C12.403 2 13 2.59695 13 3.33333V14L8 11.3333L3 14V3.33333C3 2.59695 3.59695 2 4.33333 2Z"
+        stroke="currentColor"
+        strokeWidth="1.75"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
